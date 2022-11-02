3d8bd33b79ce0bab00822c91379c53fd2a4894be
sara-elhawary <sarah.elhwarii92gmail.com>
2022-11-02 16:45:56 +0200

    console items

--- server.js
+++ server.js
@@ -6,14 +6,15 @@ const cors = require("cors");
 
 const app = express();
 const dataSet = fs.readFileSync("data.json");
+const items = JSON.parse(dataSet);
 
 app.use(cors());
 
 app.get("/", (req, res) => {
-  const items = JSON.parse(dataSet);
   res.send(items);
 });
 
 app.listen(PORT, () => {
   console.log(`Server is on http://localhost:${PORT}`);
+  console.log({ items });
 });
